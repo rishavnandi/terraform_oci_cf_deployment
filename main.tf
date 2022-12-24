59340f87471825ac135ea5c1dca70f3d6ca89438
Rishav Nandi <rishavnandi2@gmail.com>
2022-12-24 21:04:10 +0000

    Refactor code into simpler file structure

--- main.tf
+++ main.tf
@@ -1,34 +1,32 @@
-# Variables
 variable "compartment_id" { type = string }
 variable "compute_name" { type = string }
 variable "compute_subnet_id" { type = string }
 variable "compute_image_id" { type = string }
 variable "compute_ssh_authorized_keys" { type = string }
+variable "zone_id" { type = string }
+variable "record_name" { type = string }
 
 variable "compute_shape" {
   type    = string
-  default = "VM.Standard.A1.Flex"
+  default = "VM.Standard.E2.1.Micro"
 }
 
 variable "compute_cpus" {
   type    = string
-  default = "4"
+  default = "1"
 }
 
 variable "compute_memory_in_gbs" {
   type    = string
-  default = "24"
+  default = "1"
 }
 
-
-# Resources
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_id
 }
 
 resource "oci_core_instance" "tf_compute" {
-  # Required
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_id
   shape               = var.compute_shape
 
@@ -37,7 +35,6 @@ resource "oci_core_instance" "tf_compute" {
     source_type = "image"
   }
 
-  # Optional
   display_name = var.compute_name
 
   shape_config {
@@ -57,16 +54,26 @@ resource "oci_core_instance" "tf_compute" {
   preserve_boot_volume = false
 }
 
-
-# Outputs
-output "compute_id" {
-  value = oci_core_instance.tf_compute.id
+resource "cloudflare_record" "tf_compute" {
+  zone_id = var.zone_id
+  name    = var.record_name
+  type    = "A"
+  value   = oci_core_instance.tf_compute.public_ip
+  proxied = true
 }
 
-output "db_state" {
+output "compute_state" {
   value = oci_core_instance.tf_compute.state
 }
 
 output "compute_public_ip" {
   value = oci_core_instance.tf_compute.public_ip
+}
+
+output "cloudflare_record_name" {
+  value = cloudflare_record.tf_compute.name
+}
+
+output "cloudflare_record_value" {
+  value = cloudflare_record.tf_compute.value
 }
--- providers.tf
+++ providers.tf
@@ -1,16 +1,35 @@
-# Variables.
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 4.102.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.30.0"
+    }
+  }
+}
+
 variable "tenancy_ocid" { type = string }
 variable "user_ocid" { type = string }
 variable "private_key_path" { type = string }
 variable "fingerprint" { type = string }
 variable "region" { type = string }
 variable "root_compartment_id" { type = string }
+variable "cloudflare_email" { type = string }
+variable "cloudflare_api_key" { type = string }
 
-# Resources
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   private_key_path = var.private_key_path
   fingerprint      = var.fingerprint
   region           = var.region
+}
+
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }
